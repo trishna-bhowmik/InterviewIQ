b057845985703e84899a46495d389f400b8d00c9
trishna-bhowmik <thetrish474@gmail.com>
2026-07-12 22:52:09 +0530

    fix: resolve CORS callback issue

--- apps/gateway/src/app.ts
+++ apps/gateway/src/app.ts
@@ -27,7 +27,7 @@ app.use(
   cors({
     origin: (origin, callback) => {
       if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
+        return callback(null, true);
       }
 
       return callback(new Error("Not allowed by CORS"));
